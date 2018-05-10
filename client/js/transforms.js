a3c3cfba84c3ade78957cabb544f5eaa32543d84
pikulev <pikulev.vasiliy@gmail.com>
2018-05-10 14:50:13 +0300

    comments

--- client/js/transforms.js
+++ client/js/transforms.js
@@ -1,3 +1,7 @@
+/*
+ * TransformService
+ * 
+ * */
 export default class TransformService {
   constructor(UtilsService) {
     this._UtilsService = UtilsService;
@@ -5,10 +9,10 @@ export default class TransformService {
     this._dbToAppWorker = new Worker("/js/workers/transform-to-app.js");
   }
 
-  _getOnMessagePromisesFactory(worker) {
-    return () => new Promise(resolve => console.log(worker.onmessage) || (worker.onmessage = resolve)).then(event => event.data);
-  }
-
+  /*
+  * Даёт итератор по странсформированным данным для записи в БД
+  * public
+  * */
   serverToDBFormat(serverData, chunkSize) {
     const yScaleFactor = 1;
     const resultsLength = 1;
@@ -16,10 +20,20 @@ export default class TransformService {
     return this._getResultIterator(this._serverToDbWorker, serverData, yScaleFactor, chunkSize, resultsLength);
   }
 
+  /*
+  * Даёт итератор по странсформированным данным для вывода на холст
+  * public
+  * */
   dbToCanvasFormat(dbData, yScaleFactor, chunkSize) {
     return this._getResultIterator(this._dbToAppWorker, dbData, yScaleFactor, chunkSize);
   }
 
+  /*
+  * Реализует логику трансформации данных через указанный воркер
+  * Принимает также yScaleFactor, chunkSize - для масштабирования / усреднения
+  * и resultsLength - сколько результатов ожидаем (так как иногда одтаем массив, а ожидаем один объект в результате)
+  * private
+  * */
   _getResultIterator(worker, values, yScaleFactor, chunkSize = 1, resultsLength = values.length) {
     const results = this._UtilsService.getObjArray(Math.ceil(resultsLength / chunkSize));
 
@@ -31,15 +45,20 @@ export default class TransformService {
 
     worker.postMessage({ values, yScaleFactor, chunkSize });
 
+    // вернет промис
     return new Promise(resolve => {
       let resultsCounter = 0;
+
+      // отловит все сообщения от воркера
       worker.onmessage = event => {
         if (resultsCounter > results.length) {
           return;
         }
 
+        // запишет результат в буффер
         results[resultsCounter++] = event.data;
 
+        // и отдаст итератор по этому буфферу
         if (resultsCounter === results.length) {
           resolve(iterator);
         }
