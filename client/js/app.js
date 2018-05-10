77dd37f477694e3c1fcc343c357aafa33e01800e
pikulev <pikulev.vasiliy@gmail.com>
2018-05-10 14:43:32 +0300

    misprints + comments

--- client/js/app.js
+++ client/js/app.js
@@ -4,7 +4,135 @@ import TransformService from "/js/transforms.js";
 import StorageService from "/js/storage.js";
 import CanvasDrawer from "/js/canvas.js";
 
-("use strict");
+/*
+  * App - класс реализующий логику приложения
+  * 
+  * */
+class App {
+  constructor(apiService, storageService, transformService, canvasDrawer, fromSelectElement, toSelectElement) {
+    this._apiService = apiService;
+    this._storageServicePromise = storageService.init();
+    this._transformService = transformService;
+    this._canvasDrawer = canvasDrawer;
+
+    // Берем из зависимостей два selector-a
+    this._fromSelectElement = fromSelectElement;
+    this._toSelectElement = toSelectElement;
+    // Вешаем обработчики
+    this._fromSelectElement.onchange = () => this._onToOptionSelectHandler();
+    this._toSelectElement.onchange = () => this._onToOptionSelectHandler();
+    // Предзаполняем годами (чтобы не усложнять, на самом деле :)
+    this._updateOptions(1881, 2006);
+  }
+
+  /*
+  * temperatureCtrl выполняется при переходе на роут с температурой
+  * public
+  * */
+  temperatureCtrl() {
+    const TABLE_NAME = "temperature";
+    const API_PATH = "temperature";
+    const yScaleFactor = 2;
+
+    this._updateItems(TABLE_NAME, API_PATH, yScaleFactor);
+  }
+
+  /*
+  * precipitationCtrl выполняется при переходе на роут с осадками
+  * public
+  * */
+  precipitationCtrl() {
+    const TABLE_NAME = "precipitation";
+    const API_PATH = "precipitation";
+    const yScaleFactor = 6;
+
+    this._updateItems(TABLE_NAME, API_PATH, yScaleFactor);
+  }
+
+  /*
+  * _updateItems берет итератор и пробегается по нему, рисуя на холст
+  * private
+  * */
+  async _updateItems(tableName, apiPath, yScaleFactor) {
+    const iterator = await this._getItemsIterator(
+      tableName,
+      apiPath,
+      this._selectedOptions.fromValue,
+      this._selectedOptions.toValue,
+      yScaleFactor,
+      this._canvasDrawer.width
+    );
+    const drawIterator = this._canvasDrawer.draw();
+
+    for (let i of iterator()) {
+      drawIterator.next(i.value);
+    }
+
+    drawIterator.next(null);
+  }
+
+  /*
+  * _updateOptions создаёт набор опций для селекторов
+  * private
+  * */
+  _updateOptions(fromYear, toYear) {
+    const years = Array(toYear - fromYear + 1)
+      .fill()
+      .map((_, i) => fromYear + i);
+
+    const fill = (el, year) => {
+      const option = document.createElement("option");
+      // показываем данные года с первого января годя из "from"
+      option.setAttribute("data-from-value", `${year}-01-01`);
+      // и до конца декабря года в "to"
+      option.setAttribute("data-to-value", `${year}-12-31`);
+      option.innerHTML = year;
+      el.appendChild(option);
+    };
+
+    for (let i = 0; i < years.length; i++) {
+      fill(this._fromSelectElement, years[i]);
+    }
+    for (let i = years.length; i--; ) {
+      fill(this._toSelectElement, years[i]);
+    }
+
+    this._selectedOptions = {};
+  }
+
+  /*
+  * _onToOptionSelectHandler обрабатываем смену периода
+  * private
+  * */
+  _onToOptionSelectHandler() {
+    this._selectedOptions = {
+      fromValue: this._fromSelectElement.selectedOptions[0].dataset.fromValue,
+      toValue: this._toSelectElement.selectedOptions[0].dataset.toValue
+    };
+    // за это стыдно, роутер в зачатке :)
+    window.reloadCurrentRoute();
+  }
+
+  /*
+  * _getItemsIterator получаем из базы итератор и если надо синкает базу с сервером
+  * private
+  * */
+  async _getItemsIterator(tableName, apiPath, lowerKey, upperKey, yScaleFactor, averageToLimit) {
+    const storageService = await this._storageServicePromise;
+    const isSyncNeeded = await storageService.isSyncNeeded(tableName);
+
+    if (isSyncNeeded) {
+      const a = await storageService.sync(tableName, apiPath);
+    }
+
+    return storageService.getItemsIterator(tableName, lowerKey, upperKey, yScaleFactor, averageToLimit);
+  }
+}
+
+/*
+  * Исполнительная часть приложения 
+  * 
+  * */
 (w => {
   const API_URL = "/api";
   const STORE_PARAMS = {
@@ -12,103 +140,19 @@ import CanvasDrawer from "/js/canvas.js";
     version: 2
   };
 
-  class App {
-    constructor(apiService, storageService, transformService, canvasDrawer, fromSelectElement, toSelectElement) {
-      console.log("app loaded");
-      this._apiService = apiService;
-      this._storageServicePromise = storageService.init();
-      this._transformService = transformService;
-      this._canvasDrawer = canvasDrawer;
-
-      this._fromSelectElement = fromSelectElement;
-      this._toSelectElement = toSelectElement;
-      this._fromSelectElement.onchange = () => this._onToOptionSelectHandler();
-      this._toSelectElement.onchange = () => this._onToOptionSelectHandler();
-      this._updateOptions(1881, 2006);
-    }
-
-    temperatureCtrl() {
-      const TABLE_NAME = "temperature";
-      const API_PATH = "temperature";
-      const yScaleFactor = 2;
-      this._updateItems(TABLE_NAME, API_PATH, yScaleFactor);
-    }
-
-    precipitationCtrl() {
-      const TABLE_NAME = "precipitation";
-      const API_PATH = "precipitation";
-      const yScaleFactor = 6;
-      this._updateItems(TABLE_NAME, API_PATH, yScaleFactor);
-    }
-
-    async _updateItems(tableName, apiPath, yScaleFactor) {
-      const iterator = await this._getItemsIterator(
-        tableName,
-        apiPath,
-        this._selectedOptions.fromValue,
-        this._selectedOptions.toValue,
-        yScaleFactor,
-        this._canvasDrawer.width
-      );
-      const items = [];
-      const drawIterator = this._canvasDrawer.draw();
-
-      for (let i of iterator()) {
-        drawIterator.next(i.value);
-        items.push(i);
-      }
-      drawIterator.next(null);
-      console.log(items.length);
-    }
-
-    _updateOptions(fromYear, toYear) {
-      const years = Array(toYear - fromYear + 1)
-        .fill()
-        .map((_, i) => fromYear + i);
-
-      const fill = (el, year) => {
-        const option = document.createElement("option");
-        option.setAttribute("data-from-value", `${year}-01-01`);
-        option.setAttribute("data-to-value", `${year}-12-31`);
-        option.innerHTML = year;
-        el.appendChild(option);
-      };
-
-      for (let i = 0; i < years.length; i++) {
-        fill(this._fromSelectElement, years[i]);
-      }
-      for (let i = years.length; i--; ) {
-        fill(this._toSelectElement, years[i]);
-      }
-
-      this._selectedOptions = {};
-    }
-
-    _onToOptionSelectHandler() {
-      this._selectedOptions = {
-        fromValue: this._fromSelectElement.selectedOptions[0].dataset.fromValue,
-        toValue: this._toSelectElement.selectedOptions[0].dataset.toValue
-      };
-      w.reloadCurrentRoute();
-    }
-
-    async _getItemsIterator(tableName, apiPath, lowerKey, upperKey, yScaleFactor, averageToLimit) {
-      const storageService = await this._storageServicePromise;
-      const isSyncNeeded = await storageService.isSyncNeeded(tableName);
-
-      if (isSyncNeeded) {
-        const a = await storageService.sync(tableName, apiPath);
-      }
-
-      return storageService.getItemsIterator(tableName, lowerKey, upperKey, yScaleFactor, averageToLimit);
-    }
-  }
-
+  /*
+  * Берем всё нужное
+  * 
+  * */
   const utilsService = new UtilsService();
   const apiService = new ApiService(API_URL);
   const transformService = new TransformService(utilsService);
   const storageService = new StorageService(STORE_PARAMS, w.indexedDB, w.IDBKeyRange, apiService, transformService, utilsService);
 
+  /*
+  * Как только документ будет готов - отдадим сконфигурированный инстанс приложения
+  * 
+  * */
   const appPromise = new Promise(resolve => {
     w.document.addEventListener("DOMContentLoaded", () => {
       const canvasEl = w.document.getElementById("canvas");
@@ -117,6 +161,7 @@ import CanvasDrawer from "/js/canvas.js";
 
       const canvasDrawer = new CanvasDrawer(canvasEl);
 
+      // конфигурируем
       const app = new App(
         apiService,
         storageService,
@@ -126,10 +171,16 @@ import CanvasDrawer from "/js/canvas.js";
         fromSelectEl,
         toSelectEl
       );
+
+      // отдаем
       resolve(app);
     });
   });
 
+  /*
+  * Ловим события routeChanged и запускает нужный контроллер
+  * 
+  * */
   w.addEventListener("routeChanged", async event => {
     if (!event.detail.state) {
       console.error("Router error: can't find state object");
@@ -144,6 +195,7 @@ import CanvasDrawer from "/js/canvas.js";
       return;
     }
 
+    // вот тут запускает
     app[controllerName]();
   });
 })(window);
--- client/js/storage.js
+++ client/js/storage.js
@@ -1,3 +1,7 @@
+/*
+  * StorageService
+  * 
+  * */
 export default class StorageService {
   constructor({ dbName, version }, indexedDB, IDBKeyRange, ApiService, TransformService, UtilsService) {
     this.version = version;
@@ -9,6 +13,11 @@ export default class StorageService {
     this._UtilsService = UtilsService;
   }
 
+  /*
+  * Открывает БД, апгрейдит если надо, и если всё хорошо то отдаёт готовы сервис  
+  * PS: хорошо бы какую то отдельную сущность для конфигурации сделать
+  * public
+  * */
   init() {
     const DBOpenRequest = this._indexedDB.open(this.dbName, this.version);
 
@@ -16,27 +25,38 @@ export default class StorageService {
       DBOpenRequest.onerror = event => {
         throw new Error("Error loading database (opening)");
       };
+
       DBOpenRequest.onsuccess = event => {
         this._db = DBOpenRequest.result;
         this._transactoinsFactory = this._getTransactionsFactory(this._db);
         this._db.onclose = () => this.init();
+
+        // отдаём сервис, он готов
         resolve(this);
       };
+
       DBOpenRequest.onupgradeneeded = event => {
         this._onupgradeneeded(event.target.result);
       };
     });
   }
 
+  /*
+  * Берет название таблицы, range ключей, yScaleFactor - на сколько ратянуть по Y и averageToLimit - до какого объема ужать
+  * public
+  * */
   async getItemsIterator(objStoreName, lowerKey, upperKey, yScaleFactor, averageToLimit) {
+    // берем из базы все записи (месяцы)
     const tableEntries = await this._getTableEntries(objStoreName, lowerKey, upperKey);
 
+    // делаем преаллокацию и препопуляцию массива нужной длины
     const transformPomises = this._UtilsService.getObjArray(tableEntries.length);
     const chunkSize = averageToLimit ? Math.ceil(tableEntries.length * 31 / averageToLimit) : 1;
 
     console.log(chunkSize, averageToLimit, tableEntries.length);
 
     for (let i = 0; i < tableEntries.length; i++) {
+      // ждем обещания от трансформатора
       transformPomises[i] = await this._TransformService.dbToCanvasFormat(tableEntries[i].value, yScaleFactor, chunkSize);
     }
 
@@ -48,9 +68,66 @@ export default class StorageService {
       }
     };
 
+    // отдаем итератор (уже по каждой записи, не по месяцам)
     return iterator;
   }
 
+  /*
+  * Синкает таблицу с API
+  * public
+  * */
+  async sync(objStoreName, apiPath) {
+    //todo: может опустошать таблицу сначала
+    const serverData = await this._ApiService.fetch(apiPath);
+    const tableObjectIt = await this._TransformService.serverToDBFormat(serverData, 1);
+
+    // результат трансформации не массив а один обхект, поэтому просто берем результат, без итераций
+    const tableObject = tableObjectIt().next().value;
+    const keys = Object.keys(tableObject);
+    let transaction = null;
+
+    for (let lastIndex = keys.length - 1, i = 0; i < keys.length; i++) {
+      const key = keys[i];
+
+      if (i === lastIndex) {
+        transaction.onsuccess = Promise.resolve;
+      }
+
+      transaction = this._transactoinsFactory[objStoreName]();
+      transaction.add(tableObject[key], key);
+    }
+
+    // возвращаем промис последнего onsuccess - считаем, что всё завершилось удачно
+    return transaction.onsuccess;
+  }
+
+  /*
+  * Реализует критери синкать или нет. Пока смотрит просто на наличие записей в таблице
+  * public
+  * */
+  async isSyncNeeded(objStoreName) {
+    const count = await this.count(objStoreName);
+    return count === 0;
+  }
+
+  /*
+  * Отдает кол-во записей в таблице, по указанному query
+  * public
+  * */
+  count(objStoreName, query) {
+    const transaction = this._transactoinsFactory[objStoreName]();
+    const countRequest = transaction.count(query);
+    return new Promise(resolve => {
+      countRequest.onsuccess = () => {
+        resolve(countRequest.result);
+      };
+    });
+  }
+
+  /*
+  * Отдает промис на все записи из таблицы, умеет брать range по lowerKey, upperKey. 
+  * private
+  * */
   async _getTableEntries(objStoreName, lowerKey, upperKey) {
     const boundKeyRange = lowerKey && upperKey ? this._IDBKeyRange.bound(lowerKey, upperKey, false, false) : null;
 
@@ -74,44 +151,10 @@ export default class StorageService {
     });
   }
 
-  async sync(objStoreName, apiPath) {
-    //todo: опустошать таблицу сначала
-    const serverData = await this._ApiService.fetch(apiPath);
-    const tableObjectIt = await this._TransformService.serverToDBFormat(serverData, 1);
-
-    const tableObject = tableObjectIt().next().value;
-    const keys = Object.keys(tableObject);
-    let transaction = null;
-
-    for (let lastIndex = keys.length - 1, i = 0; i < keys.length; i++) {
-      const key = keys[i];
-
-      if (i === lastIndex) {
-        transaction.onsuccess = Promise.resolve;
-      }
-
-      transaction = this._transactoinsFactory[objStoreName]();
-      transaction.add(tableObject[key], key);
-    }
-
-    return transaction.onsuccess;
-  }
-
-  async isSyncNeeded(objStoreName) {
-    const count = await this.count(objStoreName);
-    return count === 0;
-  }
-
-  count(objStoreName, query) {
-    const transaction = this._transactoinsFactory[objStoreName]();
-    const countRequest = transaction.count(query);
-    return new Promise(resolve => {
-      countRequest.onsuccess = () => {
-        resolve(countRequest.result);
-      };
-    });
-  }
-
+  /*
+  * Фабрика трансакций, отдает объект с транзакцией для каждой таблицы 
+  * private
+  * */
   _getTransactionsFactory(db) {
     return Object.create(
       {},
